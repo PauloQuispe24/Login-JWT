cb942381a6a65ab042c41be13096da7963774ee3
Paulo Quispe <paulo.quispe.horna@gmail.com>
2026-08-31 22:57:30 -0500

    chore: set up Prettier and ESLint

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-    try {
-        await mongoose.connect("mongodb://mongodb:27017/login_jwt");
-        console.log("MongoDB conectado");
-    }
-    catch (error) {
-        console.error("MongoDB falló");
-        throw error;
-    }
+  try {
+    await mongoose.connect("mongodb://mongodb:27017/login_jwt");
+    console.log("MongoDB conectado");
+  } catch (error) {
+    console.error("MongoDB falló");
+    throw error;
+  }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,22 +4,21 @@ import { connectDB } from "./config/db.js";
 const app = express();
 
 app.get("/health", (req, res) => {
-    res.json({
-        status: "ok",
-        message: "Backend funcionando"
-    });
+  res.json({
+    status: "ok",
+    message: "Backend funcionando",
+  });
 });
 
 async function startServer() {
-    try {
-        await connectDB();
-        app.listen(3000, () => {
-            console.log("Nuestro Backend está funcionando en el puerto 3000.");
-        });
-    }
-    catch (error) {
-        console.error(error);
-    }
+  try {
+    await connectDB();
+    app.listen(3000, () => {
+      console.log("Nuestro Backend está funcionando en el puerto 3000.");
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 startServer();
